0e0e47e2d1b0a8e202eb0b085ac22f2011cca178
Romain <romainsaillour@orange.fr>
2018-08-07 17:03:35 +0200

    modif route log out avec method delete dans index-gossips

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+=begin
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   after_action :verify_authorized, unless: :devise_controller?
@@ -14,5 +15,5 @@ class ApplicationController < ActionController::Base
     :remember_me
     ])
   end
-
+=end
 end
