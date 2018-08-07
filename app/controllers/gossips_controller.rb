bf2ec85d2c55b930a3cc40ba025cb0d0906623eb
Romain <romainsaillour@orange.fr>
2018-08-07 20:45:13 +0200

    modif supprimer le gossip uniquement par le createur

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,5 +1,6 @@
 class GossipsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  #before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def index
     @gossips = Gossip.all
@@ -10,30 +11,39 @@ class GossipsController < ApplicationController
   end
 
   def create
-    @gossip = Gossip.new(gossip_params)
-    if @gossip.save
-      redirect_to gossips_index_path
-    end
-  end
+         puts "j'affiche le create"
+         @gossip = Gossip.create(gossip_params)
+         @gossip.user_id = current_user.id
+         if @gossip.save
+             redirect_to gossips_index_path
+         else
+             render 'new'
+         end
+     end
 
   def show
+    @gossip = Gossip.find(params[:id])
 
   end
 
   def edit
     @gossip = Gossip.find(params[:id])
+      puts 'je suis edit -------------'
   end
 
   def update
     @gossip = Gossip.find(params[:id])
-    if @gossip.update_attributes(gossip_params)
-      flash.now[:notice] = 'Gossip updated'
       redirect_to gossips_index_path
-    end
+      puts 'je suis update -------------'
   end
 
   def destroy
     @gossip = Gossip.find(params[:id])
+    unless @gossip.user_id == current_user.id
+      flash[:alert] = "You are not the creator of this gssip, therefore you're not permitted to edit or destroy this gossip"
+      redirect_to root_path # or anything you prefer
+      return false # Important to let rails know that the controller should not be executed
+    end
     @gossip.destroy
     redirect_to gossips_index_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   root to: "home#index"
+  resources :gossips
   get 'gossips/new', to: 'gossips#new'
   post 'gossips/new', to: 'gossips#create'
+  get 'gossips/new', to: 'gossips#show'
   delete 'gossips/delete', to: 'gossips#destroy'
   get 'gossips/edit/:id', to: 'gossips#edit'
   post 'gossips/edit/:id', to: 'gossips#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
