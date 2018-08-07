df85a86fa2db92f583182085e4168a07a29f60d8
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-07 14:36:39 +0200

    routes for gossips ok

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -2,4 +2,43 @@ class GossipsController < ApplicationController
   def index
     @gossips = Gossip.all
   end
+
+  def new
+    @gossip = Gossip.new
+  end
+
+  def create
+    @gossip = Gossip.new(gossip_params)
+    if @gossip.save
+      redirect_to gossips_index_path
+    end
+  end
+
+  def show
+
+  end
+
+  def edit
+    @gossip = Gossip.find(params[:id])
+  end
+
+  def update
+    @gossip = Gossip.find(params[:id])
+    if @gossip.update_attributes(gossip_params)
+      flash.now[:notice] = 'Gossip updated'
+      redirect_to gossips_index_path
+    end
+  end
+
+  def destroy
+    @gossip = Gossip.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_index_path
+  end
+
+  private
+
+  def gossip_params
+      params.require(:gossip).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   root to: "home#index"
+  get 'gossips/new', to: 'gossips#new'
+  post 'gossips/new', to: 'gossips#create'
+  delete 'gossips/delete', to: 'gossips#destroy'
+  get 'gossips/edit/:id', to: 'gossips#edit'
+  post 'gossips/edit/:id', to: 'gossips#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
